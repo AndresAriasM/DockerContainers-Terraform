2027cf55e0879d7c33152a36d9d35cd471dfc26a
AndresAriasM <andresariasmedina@hotmail.com>
2023-11-10 19:42:15 -0500

    Proyecto finalizado

--- Backend_CervezasColombia/app.py
+++ Backend_CervezasColombia/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})  # Habilita CORS para rutas que comiencen con "/api"
+CORS(app, resources={r"/api/*": {"origins": "*"}}) # Habilita CORS para rutas que comiencen con "/api"
 
 # Configura la codificación para JSON
 app.config['JSON_AS_ASCII'] = False
